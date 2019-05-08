26cbb8d178ff025fc237e49aab06619421a59e98
Alvin <alvin.frierson@fuller.edu>
2019-05-07 19:59:24 -0700

    completed doubly linked list but have a bug in the move_to_end function.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -44,6 +44,7 @@ class DoublyLinkedList:
     return self.length
 
   def add_to_head(self, value):
+      self.length += 1  
       current_head = self.head
       # If there is no head create a new ListNode and set the head and the tail to this new node.
       if not current_head:
@@ -58,22 +59,26 @@ class DoublyLinkedList:
 
   def remove_from_head(self):
         if self.head:
+            self.length -= 1
             # handle list of length 1
             if self.head == self.tail:
                 head = self.head
                 self.head = None
                 self.tail = None
+                # self.length -= 1
                 return head.value
             # else handle length > 1 list
             else:
                 old_head = self.head
                 self.head = old_head.next
                 old_head.delete()
+                # self.length -= 1
                 return old_head.value
         else:
             return None
 
   def add_to_tail(self, value):
+      self.length += 1
       current_tail = self.tail
       # If there is no element make it the current tail
       if not current_tail:
@@ -87,6 +92,7 @@ class DoublyLinkedList:
       pass
 
   def remove_from_tail(self):
+      self.length -= 1  
       if not self.tail:
           return None
       if self.head == self.tail:
@@ -101,13 +107,73 @@ class DoublyLinkedList:
           return tail.value
 
   def move_to_front(self, node):
-    pass
+        # self.length +=1
+        # check if empty
+        if self.head is not node:
+          # if in a middle spot remove the node
+            if node.next and node.prev:
+                node.delete()
+            # refer to self.head
+            current_head = self.head
+            # set current head to node since now it's in front
+            self.head = node
+            # the new head's next should be the old node that's in front of it and likewise that item's `prev` should now point to the new head which is node
+            node.next = current_head
+            current_head.prev = node
 
   def move_to_end(self, node):
-    pass
+      # check if empty
+      if self.tail is not node:
+          # if in a middle spot
+          if node.next and node.prev:
+              node.delete()
+          current_tail = self.tail
+          # the new tail's prev should be the old node that's in behind it and likewise that item's `next` should now point to the new tail which is node
+          self.tail = node
+          node.prev = current_tail
+          current_tail.prev = node
 
   def delete(self, node):
-    pass
-    
+      self.length -= 1  
+      # check if empty and return None if it is
+      if not self.head and not self.tail:
+          return None
+      # if the list only has a single node, delete it
+      # both self.head and self.tail should be None
+      if self.head == self.tail:
+          node.delete()
+          self.head = None
+          self.tail = None
+      # if given node is the head
+      if self.head == node:
+          # set the self.head pointer to the next node
+          self.head = node.next
+          # delete the node
+          node.delete()
+
+      # if the given node is the tail
+      if self.tail == node:
+          # set the self.tail pointer to the previous node
+          self.tail = node.prev
+      # delete the node
+          node.delete()
+      # otherwise, tnode is somewhere in the middle and we can delete normally
+      else:
+          # just call node.delete
+          node.delete()
+
   def get_max(self):
-    pass
+      max = 0
+      # Start at the beginning looking for the max value and return that value
+      current = self.head
+      while current:
+          if current.value > max:
+              max = current.value
+          current = current.next
+      return max
+
+# dll = DoublyLinkedList(ListNode(1))
+
+# dll.move_to_end(ListNode(40))
+
+# print(len(dll))
